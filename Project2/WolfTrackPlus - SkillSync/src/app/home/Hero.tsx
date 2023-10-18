a456dbf71c6c95268333c266ffa6a3ec6fe0b523
Sahil Changlani <sahilchanglani@gmail.com>
2023-10-17 20:40:33 -0400

    design changes

--- Project2/WolfTrackPlus - SkillSync/src/app/home/Hero.tsx
+++ Project2/WolfTrackPlus - SkillSync/src/app/home/Hero.tsx
@@ -7,18 +7,18 @@ export const Hero = () => {
     <section className="lg:flex lg:h-[825px] lg:justify-center">
       <FlexboxSpacer maxWidth={75} minWidth={0} className="hidden lg:block" />
       <div className="mx-auto max-w-xl pt-8 text-center lg:mx-0 lg:grow lg:pt-32 lg:text-left">
-        <h1 className="text-primary pb-2 text-4xl font-bold lg:text-5xl">
+        <h1 className="text-primary pb-2 text-4xl font-bold lg:text-5xl">WolfTrack Plus - SkillSync</h1>
+        <h2 className="text-secondary pb-2 text-2xl lg:text-5xl">
           Create a professional
           <br />
           resume easily
-        </h1>
+        </h2>
         <p className="mt-3 text-lg lg:mt-5 lg:text-xl">
           With this free, open-source, and powerful resume builder
         </p>
         <Link href="/resume-import" className="btn-primary mt-6 lg:mt-14">
           Create Resume <span aria-hidden="true">→</span>
         </Link>
-        <p className="ml-6 mt-3 text-sm text-gray-600">No sign up required</p>
         <p className="mt-3 text-sm text-gray-600 lg:mt-36">
           Already have a resume? Test its ATS readability with the{" "}
           <Link href="/resume-parser" className="underline underline-offset-2">
